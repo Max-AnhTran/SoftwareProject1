0d3cc18e5e4121920b6e03fefacaf78d71277aa7
Anh Tran <bhm349@myy.haaga-helia.fi>
2025-04-02 12:53:58 +0300

    first commit

---
+++ Backend/src/main/java/haaga_helia/fi/SoftwareProject1/SoftwareProject1Application.java
@@ -0,0 +1,13 @@
+package haaga_helia.fi.SoftwareProject1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareProject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftwareProject1Application.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/haaga_helia/fi/SoftwareProject1/SoftwareProject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package haaga_helia.fi.SoftwareProject1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftwareProject1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
