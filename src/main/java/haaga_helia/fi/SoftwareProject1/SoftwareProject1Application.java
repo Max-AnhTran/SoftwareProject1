cca41d5061a90264d3c20de02797f2cc8965957d
Anh Tran <bhm349@myy.haaga-helia.fi>
2025-04-15 17:51:12 +0300

    fix pom and separate database loader to a new file

--- src/main/java/haaga_helia/fi/SoftwareProject1/SoftwareProject1Application.java
+++ src/main/java/haaga_helia/fi/SoftwareProject1/SoftwareProject1Application.java
@@ -1,37 +1,12 @@
 package haaga_helia.fi.SoftwareProject1;
 
-
-
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.context.annotation.Bean;
-import haaga_helia.fi.SoftwareProject1.domain.QuizRepository;
-import haaga_helia.fi.SoftwareProject1.domain.CategoryRepository;
-import haaga_helia.fi.SoftwareProject1.domain.Quiz;
-import haaga_helia.fi.SoftwareProject1.domain.Category;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;	
 
 @SpringBootApplication
-public class SoftwareProject1Application extends SpringBootServletInitializer{
+public class SoftwareProject1Application {
 
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(SoftwareProject1Application.class);
-	}
 	public static void main(String[] args) {
 		SpringApplication.run(SoftwareProject1Application.class, args);
 	}
-
-	@Bean
-	public CommandLineRunner demo(QuizRepository qrepository, CategoryRepository crepository) {
-		return (args) -> {
-			Category category1 = new Category("example", "example");
-			crepository.save(category1);
-			Quiz quiz1 = new Quiz("example", "something", "who-cares", false, category1);
-			qrepository.save(quiz1);
-		};
-	}
-
 }
